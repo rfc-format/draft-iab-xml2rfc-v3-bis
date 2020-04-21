0c5d9bcb8d3d0eee98ecc5333da37f945b87047b
Julian Reschke <julian.reschke@gmx.de>
2020-04-21 11:25:07 +0100

    add missing include directive for SVG element (#11)

--- fold-rnc.sh
+++ fold-rnc.sh
@@ -4,4 +4,5 @@ sed 's|^\([-a-zA-Z0-9\]*\)* =|\
 <strong anchor="grammar.\1">\1</strong><iref item="\1 element"/> =|' | \
 sed 's|anchor="grammar\.\\|anchor="grammar\.|' | \
 sed 's|item="\\|item="|' | \
+sed 's|see Appendix A of \[RFC7996\]|see <xref target="RFC7996" x:fmt="of" x:sec="A"/>|' | \
 sed 's|<strong anchor="grammar.start">start</strong><iref item="start element"/>|start|'
